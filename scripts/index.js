338464d80ee444f035241ed086e1dc26f993ac53
HarleyB123-LCWC <144430651+HarleyB123-LCWC@users.noreply.github.com>
2023-12-08 16:11:02 +0000

    Initial

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+
+function Home(){
+    var element = document.querySelector("#home")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function Malware(){
+    var element = document.querySelector("#malware")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function CyberSec(){
+    var element = document.querySelector("#cybersec")
+    element.scrollIntoView({behavior: "smooth"});
+}
+function AboutCyber(){
+    var element = document.querySelector("#aboutcyber")
+    element.scrollIntoView({behavior: "smooth"});
+}
